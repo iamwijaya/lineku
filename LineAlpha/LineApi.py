66e69f8537110b9c06b81b3174eda356f95d12f6
iamwijaya <31705906+iamwijaya@users.noreply.github.com>
2018-01-13 12:03:21 +0800

    Update LineApi.py

--- LineAlpha/LineApi.py
+++ LineAlpha/LineApi.py
@@ -179,7 +179,7 @@ class LINE:
         return self.Talk.client.getMessageBoxWrapUpList(start, messageBoxCount)
   
   def removeAllMessages(self, lastMessageId):
-     return self.client.removeAllMessages(0, lastMessageId)
+     return self.Talk.client.removeAllMessages(0, lastMessageId)
 
   """Contact"""
 
